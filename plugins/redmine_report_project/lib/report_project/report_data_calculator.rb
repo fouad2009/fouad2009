83c761ff34b90c12b309d39bc27faae0914f5ad6
fouad2009 <32742558+fouad2009@users.noreply.github.com>
2025-12-21 21:59:33 +0100

    Refactor synthese_data_issues method v1
    Refactor synthese_data_issues method to retrieve tracker_id from issue directly and update tracker handling.

--- plugins/redmine_report_project/lib/report_project/report_data_calculator.rb
+++ plugins/redmine_report_project/lib/report_project/report_data_calculator.rb
@@ -1,21 +1,35 @@
 # lib/report_project/report_data_calculator.rb
 module ReportProject
   module ReportDataCalculator
-    def self.synthese_data_issues(issue, tracker_struct, tracker_id)
+    def self.synthese_data_issues(issue, tracker_struct)
+      tracker_id = issue[:tracker_id]  # récupéré directement depuis l'issue
       # Logique métier selon le tracker
       case tracker_id
-      when ReportProject::ReportSchema::TRACKERS[:canalisation]
+        when ReportProject::ReportSchema::TRACKERS[:aps]
+        when ReportProject::ReportSchema::TRACKERS[:odn_mod]
+        when ReportProject::ReportSchema::TRACKERS[:odn_dev]
+    
+        when ReportProject::ReportSchema::TRACKERS[:odn_service]
+        
+        when ReportProject::ReportSchema::TRACKERS[:canalisation]
         # Calcul spécifique Canalisation
-      when ReportProject::ReportSchema::TRACKERS[:pose_fo]
-        # Calcul spécifique Pose FO
+        when ReportProject::ReportSchema::TRACKERS[:pose_fo]
+        when ReportProject::ReportSchema::TRACKERS[:pa_4g]
+	    when ReportProject::ReportSchema::TRACKERS[:olt]
+	    when ReportProject::ReportSchema::TRACKERS[:carte_gpon]
+	    when ReportProject::ReportSchema::TRACKERS[:corporate]
+	    when ReportProject::ReportSchema::TRACKERS[:depose_cable]
       else
         # Calcul générique
       end
-
-      # Mise à jour des métriques dans tracker_struct
-      tracker_struct[:planned][:count] += 1 if issue[:status_id] == 1
-      tracker_struct[:completed_pa][:quantity] += issue[:done_ratio]
       # etc...
-    end
   end
-end
+
+  def normalize_aps(
+
+  end
+
+	  
+  
+end  
+end 
