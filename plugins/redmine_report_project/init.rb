c74dc1dbefa21c533d01113998930ed687995b8d
fouad2009 <32742558+fouad2009@users.noreply.github.com>
2026-01-02 10:53:06 +0000

    mise a jout Init.rb

--- plugins/redmine_report_project/init.rb
+++ plugins/redmine_report_project/init.rb
@@ -5,12 +5,23 @@ Redmine::Plugin.register :redmine_report_project do
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
+end
 
 plugin_root = File.expand_path(__dir__)
-config_file = File.join(plugin_root, 'config', 'custom_fields.yml')
 
-CUSTOM_FIELDS_CONFIG = YAML.load_file(config_file).with_indifferent_access.freeze
-
- Dir[File.join(__dir__, 'lib', 'report_project', '*.rb')].each { |file| require file }
-
+def load_yaml_config(path)
+  YAML.safe_load(
+    File.read(path),
+    permitted_classes: [Symbol],
+    aliases: true
+  ).with_indifferent_access.freeze
 end
+
+CUSTOM_FIELDS_CONFIG =
+  load_yaml_config(File.join(plugin_root, 'config', 'custom_fields.yml'))
+
+RULES_NBR_CONFIG =
+  load_yaml_config(File.join(plugin_root, 'config', 'rules_nbr.yml'))
+
+RULES_QTN_CONFIG =
+  load_yaml_config(File.join(plugin_root, 'config', 'rules_qtn.yml'))
