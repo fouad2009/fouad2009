98a53a7ef6ddacb254a31f0b39dbe0fcf623efa6
fouad2009 <32742558+fouad2009@users.noreply.github.com>
2026-01-03 14:55:44 +0000

    détection erreurs sur Aps class

--- plugins/redmine_report_project/lib/report_project/aps_module.rb
+++ plugins/redmine_report_project/lib/report_project/aps_module.rb
@@ -8,30 +8,31 @@ module ReportProject
       # Methodes de traitement par action 
       # =========================
 
-        config_aps = RULES_NBR_CONFIG[:actions][:aps]
+    RULES_NBR_CONFIG[:actions][:aps].each do |action, cfg|
+  state = cfg[:state]
+  field = cfg[:field]
 
-        if data[:tracker_id] == 29  # meme deja dispatcher a filtrer l'envoi selon le type tracker_id , appliqué un deuxiem  filtre
-             
-             begin
+  [:pa, :hp, :rar].each do |type|
+    if struct_tracker[state].nil?
+      Rails.logger.error "Nil detected! struct_tracker[state] est nil pour state=#{state}, action=#{action}"
+      next
+    end
 
-              config_aps.each do |status,value|
-                value.each do |state,field|
-                  if  field == :count 
-                   tracker_struct[state][:pa][field] += 1 if data[:exercice_PA]
-                   tracker_struct[state][:hp][field] += 1 if data[:exercice_HP]
-                   tracker_struct[state][:rar][field]+=1 if data[:exercice_RAR]
-                  end
-                end
-              end
+    if struct_tracker[state][type].nil?
+      Rails.logger.error "Nil detected! struct_tracker[state][type] est nil pour state=#{state}, type=#{type}, action=#{action}"
+      next
+    end
 
-             rescue => e
-                    Rails.logger.error "Une erreur est survenue: #{e.message}"
-                   Rails.logger.error e.backtrace.join("\n")  # pour avoir la stack complète
-             end
-                  
-        end
-             
-        
+    unless struct_tracker[state][type].key?(field)
+      Rails.logger.error "Nil detected! struct_tracker[state][type] n'a pas la clé field=#{field} pour action=#{action}"
+      next
+    end
+
+    # Si tout est OK, incrémente
+    struct_tracker[state][type][field] ||= 0
+    struct_tracker[state][type][field] += 1
+  end
+end
 
 
       end
