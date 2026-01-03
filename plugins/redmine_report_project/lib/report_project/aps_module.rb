5be7c533df5bd4a4caa5934d464cd0e113fd5a64
fouad2009 <32742558+fouad2009@users.noreply.github.com>
2026-01-03 11:44:01 +0000

    update Aps class

--- plugins/redmine_report_project/lib/report_project/aps_module.rb
+++ plugins/redmine_report_project/lib/report_project/aps_module.rb
@@ -8,7 +8,7 @@ module ReportProject
       # Methodes de traitement par action 
       # =========================
 
-        config_aps = RULES_NBR_CONFIG[:action][:aps]
+        config_aps = RULES_NBR_CONFIG[:actions][:aps]
 
         if data[:tracker_id] == 29  # meme deja dispatcher a filtrer l'envoi selon le type tracker_id , appliqué un deuxiem  filtre
 
