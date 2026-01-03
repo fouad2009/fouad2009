ccc8717d1e9b3b39f323d06d705cf476067188f3
fouad2009 <32742558+fouad2009@users.noreply.github.com>
2026-01-03 15:20:38 +0000

    commit gestion erreurs

--- plugins/redmine_report_project/lib/report_project/aps_module.rb
+++ plugins/redmine_report_project/lib/report_project/aps_module.rb
@@ -7,30 +7,30 @@ module ReportProject
       # =========================
       # Methodes de traitement par action 
       # =========================
-
+      
     RULES_NBR_CONFIG[:actions][:aps].each do |action, cfg|
   state = cfg[:state]
   field = cfg[:field]
 
   [:pa, :hp, :rar].each do |type|
-    if struct_tracker[state].nil?
-      Rails.logger.error "Nil detected! struct_tracker[state] est nil pour state=#{state}, action=#{action}"
+    if tracker_struct[state].nil?
+      Rails.logger.error "Ntracker_structtruct_tracker[state] est nil pour state=#{state}, action=#{action}"
       next
     end
 
-    if struct_tracker[state][type].nil?
-      Rails.logger.error "Nil detected! struct_tracker[state][type] est nil pour state=#{state}, type=#{type}, action=#{action}"
+    if tracker_struct[state][type].nil?
+      Rails.logger.error "Ntracker_structtruct_tracker[state][type] est nil pour state=#{state}, type=#{type}, action=#{action}"
       next
     end
 
-    unless struct_tracker[state][type].key?(field)
-      Rails.logger.error "Nil detected! struct_tracker[state][type] n'a pas la clé field=#{field} pour action=#{action}"
+    unless tracker_struct[state][type].key?(field)
+      Rails.logger.error "Ntracker_structtruct_tracker[state][type] n'a pas la clé field=#{field} pour action=#{action}"
       next
     end
 
     # Si tout est OK, incrémente
-    struct_tracker[state][type][field] ||= 0
-    struct_tracker[state][type][field] += 1
+    tracker_struct[state][type][field] ||= 0
+    tracker_struct[state][type][field] += 1
   end
 end
 
